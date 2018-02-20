4c87948928c99710aa3781cfdca67c829fb5df65
Jim Anders <jim@janders223.com>
2018-02-20 09:44:24 -0500

    Setup linting and testing

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": ["airbnb-base", "prettier",],
+    "plugins": ["prettier",],
+    "env": {
+        "node": true,
+        "jest": true,
+    },
+};
---
+++ src/__test__/index.test.js
@@ -0,0 +1,6 @@
+import ServerlessOfflineSSM from '../'
+
+test('it works', () => {
+    new ServerlessOfflineSSM() // eslint-disable-line
+    expect((2 - 1) === 1).toBeTruthy()
+})
