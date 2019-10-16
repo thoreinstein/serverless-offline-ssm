82cf0561e6a02d8a844a78425e1e1aa285473a98
Barend Bootha <barend.bootha@gmail.com>
2019-10-17 09:54:58 +1300

    dists :computer:

--- dist/index.js
+++ dist/index.js
@@ -7,19 +7,40 @@ class ServerlessOfflineSSM {
         if (this.serverless.processedInput.commands.indexOf('offline') < 0) {
             return;
         }
-        this.variables = serverless.variables;
-        this.ssmRefSyntax = RegExp(/^(?:\${)?ssm:([a-zA-Z0-9_.\-/]+)[~]?(true|false|split)?/);
-        const config = this.getConfigFromServerlessYml();
-        this.serverless.variables.getValueFromSsm = function getValueFromSsm(variable) {
-            const param = variable.match(this.ssmRefSyntax)[1];
-            const vars = Object.keys(config).length === 0
+        if (!this.checkCompatibility()) {
+            console.log('This version of the plugin only works with Serverless 1.52 upwards.');
+            return;
+        }
+        const serverlessVars = this.serverless.variables;
+        const customConfig = this.getConfigFromServerlessYml();
+        serverlessVars.getValueFromSsmOffline = (variable) => {
+            const param = variable.match(serverlessVars.ssmRefSyntax)[1];
+            const vars = Object.keys(customConfig).length === 0
                 ? util_1.getVarsFromEnv()
-                : config;
+                : customConfig;
             return Promise.resolve(vars[param]);
         };
+        // override the ssm resolver function
+        for (const varResolver of serverlessVars.variableResolvers) {
+            if (varResolver.serviceName === 'SSM') {
+                varResolver.resolver = serverlessVars.getValueFromSsmOffline.bind(this);
+            }
+        }
     }
     getConfigFromServerlessYml() {
         return (this.serverless.service.custom || {})['serverless-offline-ssm'] || {};
+    }
+    /**
+     * This plugin is only compatible with serverless 1.52+
+     */
+    checkCompatibility() {
+        const semver = this.serverless.version
+            .split('.')
+            .map(i => Number(i));
+        if (semver[0] <= 1 && semver[1] < 52) {
+            return false;
+        }
+        return true;
     }
 }
 module.exports = ServerlessOfflineSSM;
